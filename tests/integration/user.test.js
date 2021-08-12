0882d63e6cb830bd32d8160b19c284fffc863a49
Marcelo Gonçalves <marcelo_goncalves@id.uff.br>
2021-08-12 11:39:44 -0300

    Tests: Added response.body tests

--- tests/integration/user.test.js
+++ tests/integration/user.test.js
@@ -21,124 +21,136 @@ describe("POST /register", () => {
   it("returns status 201 for valid params", async () => {
     const body = createUser('validname', 'validmail@gmail.com', 'validpassword');
 
-    const test = await agent.post("/register").send(body);
+    const request = await agent.post("/register").send(body);
 
-    expect(test.status).toEqual(201);
+    expect(request.status).toEqual(201);
   });
 
   it("returns status 400 for empty name", async () => {
     const body = createUser('', 'validmail@gmail.com', 'validpassword');
 
-    const test = await agent.post("/register").send(body);
+    const request = await agent.post("/register").send(body);
 
-    expect(test.status).toEqual(400);
+    expect(request.status).toEqual(400);
   });
 
   it("returns status 400 for invalid format email", async () => {
     const body = createUser('validname', 'invalidmailformat', 'validpassword');
 
-    const test = await agent.post("/register").send(body);
+    const request = await agent.post("/register").send(body);
 
-    expect(test.status).toEqual(400);
+    expect(request.status).toEqual(400);
   });
 
   it("returns status 400 for empty email", async () => {
     const body = createUser('validname', '', 'validpassword');
 
-    const test = await agent.post("/register").send(body);
+    const request = await agent.post("/register").send(body);
 
-    expect(test.status).toEqual(400);
+    expect(request.status).toEqual(400);
   });
 
   it("returns status 400 for password with less than 4 characters", async () => {
     const body = createUser('validname', 'validmail@gmail.com', 'abc');
 
-    const test = await agent.post("/register").send(body);
+    const request = await agent.post("/register").send(body);
     
-    expect(test.status).toEqual(400);
+    expect(request.status).toEqual(400);
   });
 
   it("returns status 409 for email already registered", async () => {
     const body = createUser('validname', 'validmail@gmail.com', 'validpassword');
 
-    const test = await agent.post("/register").send(body);
+    const request = await agent.post("/register").send(body);
 
-    expect(test.status).toEqual(201);
+    expect(request.status).toEqual(201);
 
-    const exist = await agent.post("/register").send(body);
+    const exists = await agent.post("/register").send(body);
     
-    expect(exist.status).toEqual(409);
+    expect(exists.status).toEqual(409);
   });
 });
 
 describe("POST /login", () => {
+
+  const emailPattern = '[a-zA-Z0-9+._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+';
+  const tokenPattern = '[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}';
+
   it("returns status 200 for valid params", async () => {
     const body = createUser('validname', 'validmail@gmail.com', 'validpassword');
 
-    const test = await agent.post("/register").send(body);
+    const request = await agent.post("/register").send(body);
 
     const login = createLogin('validmail@gmail.com', 'validpassword');
 
-    const exist = await agent.post("/login").send(login);
+    const exists = await agent.post("/login").send(login);
 
-    expect(test.status).toEqual(201);
+    expect(request.status).toEqual(201);
 
-    expect(exist.status).toEqual(200);
+    expect(exists.status).toEqual(200);
+
+    expect(exists.body).toEqual(expect.objectContaining({
+      id: expect.any(Number),
+      nome: expect.any(String),
+      email: expect.stringMatching(emailPattern),
+      token: expect.stringMatching(tokenPattern),
+    }));
+
   });
 
   it("returns status 400 for invalid email format", async () => {
     const body = createUser('validname', 'validmail@gmail.com', 'validpassword');
 
-    const test = await agent.post("/register").send(body);
+    const request = await agent.post("/register").send(body);
 
     const login = createLogin('invalidmailformat', 'validpassword');
 
-    const exist = await agent.post("/login").send(login);
+    const exists = await agent.post("/login").send(login);
 
-    expect(test.status).toEqual(201);
+    expect(request.status).toEqual(201);
     
-    expect(exist.status).toEqual(400);
+    expect(exists.status).toEqual(400);
   });
 
   it("returns status 400 for password with less than 4 characters", async () => {
     const body = createUser('validname', 'validmail@gmail.com', 'validpassword');
 
-    const test = await agent.post("/register").send(body);
+    const request = await agent.post("/register").send(body);
 
     const login = createLogin('invalidmailformat', 'abc');
 
-    const exist = await agent.post("/login").send(login);
+    const exists = await agent.post("/login").send(login);
 
-    expect(test.status).toEqual(201);
+    expect(request.status).toEqual(201);
     
-    expect(exist.status).toEqual(400);
+    expect(exists.status).toEqual(400);
   });
 
   it("returns status 401 for unregistered email", async () => {
     const body = createUser('validname', 'validmail@gmail.com', 'validpassword');
 
-    const test = await agent.post("/register").send(body);
+    const request = await agent.post("/register").send(body);
 
     const login = createLogin('validmail2@gmail.com', 'validpassword');
 
-    const exist = await agent.post("/login").send(login);
+    const exists = await agent.post("/login").send(login);
 
-    expect(test.status).toEqual(201);
+    expect(request.status).toEqual(201);
     
-    expect(exist.status).toEqual(401);
+    expect(exists.status).toEqual(401);
   });
 
   it("returns status 401 for unregistered password", async () => {
     const body = createUser('validname', 'validmail@gmail.com', 'validpassword');
 
-    const test = await agent.post("/register").send(body);
+    const request = await agent.post("/register").send(body);
 
     const login = createLogin('validmail@gmail.com', 'validpassword2');
 
-    const exist = await agent.post("/login").send(login);
+    const exists = await agent.post("/login").send(login);
 
-    expect(test.status).toEqual(201);
+    expect(request.status).toEqual(201);
     
-    expect(exist.status).toEqual(401);
+    expect(exists.status).toEqual(401);
   });
 });
